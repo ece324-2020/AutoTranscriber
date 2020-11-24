d859d37b9eff7d6856e8be2178291502ab54e449
Anthony Lem <anthonyjlem@gmail.com>
2020-11-23 20:34:17 -0700

    added python file to make data loaders

--- train_val_loaders.py
+++ train_val_loaders.py
@@ -2,6 +2,8 @@
 
 import numpy as np
 import os
+import pretty_midi
+from torch.utils.data import Dataset, DataLoader
 
 def get_statistics(train_path):
 
@@ -39,3 +41,119 @@ def get_statistics(train_path):
 
     return mean, std
 
+class _MusicDataset(Dataset):
+
+    '''
+    A Dataset object for music that returns a 2D spectrogram and its
+    corresponding MIDI file when used in a DataLoader.
+
+    Attributes
+    ----------
+    dir (str): Path to where all of the data (npy files) is. It is expected to
+        end with 'data'.
+    list_of_data (list): List of all of the data located at dir
+
+    Methods
+    -------
+    __len__():
+        Returns the number of npy files in self.dir.
+    __getitem__(index):
+        Returns a data sample and its label from self.dir at index index.
+    '''
+
+    def __init__(self, directory):
+
+        '''
+        Initializes all of the attributes.
+
+            Parameters:
+                directory (str): Path to all of the data (npy files).
+        '''
+        
+        assert type(directory) = str, 'Input argument must be a string.'
+        # Store directory
+        count = 0 # number of slashes at the end of directory
+        for i in range(len(directory)-1,0,-1):
+            if directory[i] == '/':
+                count += 1
+            elif directory[i] == '\\':
+                count += 1
+            else:
+                break
+        self.dir = directory[:len(directory)-count]
+        assert self.dir[-4:] == 'data', 'Input argument must end with "data".'
+        assert os.path.isdir(self.dir) == True, 'Input argument must be a valid directory.'
+        assert os.path.isdir(self.dir[:-4]+'labels') == True, 'There must be a corresponding "labels" directory.'
+
+        self.list_of_data = [i if i[-3:] == 'npy' for i in os.listdir(directory)]
+        
+    def __len__(self):
+
+        '''
+        Returns the number of npy files in self.dir.
+        '''
+
+        return len(self.list_of_data)
+
+    def __getitem__(self, index):
+
+        '''
+        Returns a data sample and its label from self.dir at index index.
+
+            Parameters:
+                index (int): The data sample to get.
+
+            Returns:
+                data_sample (numpy.ndarray): A spectrogram
+                data_label (numpy.ndarray): A piano roll
+        '''
+
+        assert type(index) == int, 'Input argument must be an integer'
+        # Get data sample
+        data_sample = np.load(os.path.join(self.dir,self.list_of_data[index]))
+        # Load data label
+        midi = pretty_midi.PrettyMIDI(os.path.join(self.dir[:-4]+'labels',
+                                                   self.list_of_data[index][:-4]+'mid'))
+        data_label = midi.get_piano_roll()
+
+        return data_sample, data_label
+
+def get_data_loaders(train_path, val_path, test_path, batch_size=1,
+                     shuffle=True):
+
+    '''
+    Returns data loaders to use for training a network with music datasets.
+
+        Parameters:
+            train_path (str): Path to directory containing all of the training
+                data as .npy files
+            val_path (str): Path to directory containing all of the validation
+                data as .npy files
+            test_path (str): Path to directory containing all of the testing
+                data as .npy files
+            batch_size (int): Batch size to use when loading data
+            shuffle (bool): If data when loaded should be shuffled or not
+            
+        Returns:
+            train_loader (DataLoader): Data loader with all of the training data
+            val_loader (DataLoader): Data loader with all of the validation
+                data
+            test_loader (DataLoader): Data loader with all of the testing data
+    '''
+
+    assert os.path.isdir(train_path) == True, 'Training path must be a valid directory.'
+    assert os.path.isdir(val_path) == True, 'Validation path must be a valid directory.'
+    assert os.path.isdir(test_path) == True, 'Testing path must be a valid directory.'
+    assert type(batch_size) == int, 'Batch_size must be an integer.'
+    assert batch_size > 0, 'Batch_size must be greater than 0.'
+    assert type(shuffle) == bool, 'Shuffle must be a boolean.'
+
+    train_set = _MusicDataset(train_path)
+    val_set = _MusicDataset(val_path)
+    test_set = _MusicDataset(test_path)
+
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=shuffle)
+    val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=shuffle)
+    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=shuffle)
+
+    return train_loader, val_loader, test_loader
