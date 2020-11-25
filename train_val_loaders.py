eae4869c9f06f38f048a8cd84ee873f799350a0c
Anthony Lem <anthonyjlem@gmail.com>
2020-11-25 12:14:59 -0700

    normalized data in train_val_loaders.py

--- train_val_loaders.py
+++ train_val_loaders.py
@@ -10,7 +10,7 @@ Usage:
     mean, std = get_statistics("data_path")
     # data_path should be the directory that contains all of the npy files
 
-    train_loader, val_loader, test_loader = get_data_loaders("train_path", "val_path", "test_path", batch_size=batch_size, shuffle=True)
+    train_loader, val_loader, test_loader = get_data_loaders("train_path", "val_path", "test_path", batch_size=batch_size, shuffle=True, normalize=True, mean=mean, std=std)
     # You can load data from train_path and/or val_path and/or test_path.
     # The paths should point to a directory with a 'data' and 'labels' folder inside.
     for data, label in train_loader:
@@ -70,6 +70,9 @@ class _MusicDataset(Dataset):
         end with 'data'.
     list_of_data (list): List of all of the data located at dir
     fs (int/float): Sampling frequency to use for MIDI file
+    normalize (boolean): Specify to normalize data
+    mean (int/float): Mean to use to normalize data
+    std (int/float): Standard deviation to use to normalize data
 
     Methods
     -------
@@ -79,7 +82,7 @@ class _MusicDataset(Dataset):
         Returns a data sample and its label from self.dir at index index.
     '''
 
-    def __init__(self, directory, sample_freq=31.3):
+    def __init__(self, directory, sample_freq=31, normalize=False, mean=None, std=None):
 
         '''
         Initializes all of the attributes.
@@ -103,12 +106,19 @@ class _MusicDataset(Dataset):
         assert os.path.isdir(os.path.join(self.dir,'data')) == True, 'There must be a "data" directory within the specified path.'
         assert os.path.isdir(os.path.join(self.dir,'labels')) == True, 'There must be a "labels" directory within the specified path.'
         assert type(sample_freq) == int or type(sample_freq) == float, 'Keyword argument "sample_freq" must be an integer or float.'
+        assert type(normalize) == bool, 'Keyword argument "normalize" must be a boolean.'
+        if normalize:
+            assert type(mean) == int or type(mean) == float, 'Keyword argument "mean" must be an integer or float.'
+            assert type(std) == int or type(std) == float, 'Keyword argument "std" must be an integer or float.'
 
         self.list_of_data = []
         for i in os.listdir(os.path.join(self.dir,'data')):
             if i[-3:] == 'npy':
                 self.list_of_data.append(i)
         self.fs = sample_freq
+        self.normalize = normalize
+        self.mean = mean
+        self.std = std
         
     def __len__(self):
 
@@ -134,15 +144,18 @@ class _MusicDataset(Dataset):
         assert type(index) == int, 'Input argument must be an integer'
         # Get data sample
         data_sample = np.load(os.path.join(self.dir,'data',self.list_of_data[index]))
+        if self.normalize:
+            data_sample = (data_sample-self.mean)/self.std
         # Load data label
         midi = pretty_midi.PrettyMIDI(os.path.join(self.dir,'labels',
                                                    self.list_of_data[index][:-3]+'mid'))
-        data_label = midi.get_piano_roll(fs=self.fs)[21:109,:]
+        piano_roll = midi.get_piano_roll(fs=self.fs)[21:109,:]
+        data_label = np.where(piano_roll > 0, 1, 0)
 
         return data_sample, data_label
 
 def get_data_loaders(train_path, val_path=None, test_path=None, batch_size=1,
-                     shuffle=True):
+                     shuffle=True, normalize=False, mean=None, std=None):
 
     '''
     Returns data loaders to use for training a network with music datasets.
@@ -156,6 +169,9 @@ def get_data_loaders(train_path, val_path=None, test_path=None, batch_size=1,
                 data as .npy files
             batch_size (int): Batch size to use when loading data
             shuffle (bool): If data when loaded should be shuffled or not
+            normalize (bool): If data should be normalized
+            mean (int/float): Mean to use to normalize data
+            std (int/float): Standard deviation to use to normalize data
             
         Returns:
             train_loader (DataLoader): Data loader with all of the training data
@@ -172,14 +188,18 @@ def get_data_loaders(train_path, val_path=None, test_path=None, batch_size=1,
     assert type(batch_size) == int, 'Batch_size must be an integer.'
     assert batch_size > 0, 'Batch_size must be greater than 0.'
     assert type(shuffle) == bool, 'Shuffle must be a boolean.'
+    assert type(normalize) == bool, 'Normalize must be a boolean.'
+    if normalize:
+        assert type(mean) == int or type(mean) == float, 'Mean must be an integer or float.'
+        assert type(std) == int or type(std) == float, 'Std must be an integer or float.'
 
-    train_set = _MusicDataset(train_path)
+    train_set = _MusicDataset(train_path, normalize=normalize, mean=mean, std=std)
     train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=shuffle)
     if val_path != None:
-        val_set = _MusicDataset(val_path)
+        val_set = _MusicDataset(val_path, normalize=normalize, mean=mean, std=std)
         val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=shuffle)
     if test_path != None:
-        test_set = _MusicDataset(test_path)
+        test_set = _MusicDataset(test_path, normalize=normalize, mean=mean, std=std)
         test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=shuffle)
 
     if val_path == None and test_path == None:
