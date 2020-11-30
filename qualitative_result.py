ba15c70b7f97e0246851b2ad91c624e91375e663
Anthony Lem <anthonyjlem@gmail.com>
2020-11-30 13:28:08 -0700

    cleaned up qualitative_result.py

--- qualitative_result.py
+++ qualitative_result.py
@@ -75,11 +75,8 @@ def qualitative_result(net, path, rand=True):
             if path[i] == '/' or path[i] == '\\':
                 break
 
-##        print(i)
         data_file = path
         data_sample = np.load(path)
-##        print(path)
-##        print(os.path.join(path[:i],'labels',path[i+1:-4]+'.mid'))
         midi = pretty_midi.PrettyMIDI(path[:i-5]+'/labels'+path[i:-4]+'.mid') # assume data_file ends with a three letter file type
         label_sample = midi.get_piano_roll(fs=31)[21:109,:124]
         label_sample = np.where(label_sample > 0, 1, 0)
