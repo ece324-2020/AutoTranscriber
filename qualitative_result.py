137a9ba1f1f343c6ed1bca5e2b15677282e7b76d
RickyY1689 <richard.jr.yang@gmail.com>
2020-12-03 22:59:37 -0500

    updating files

--- qualitative_result.py
+++ qualitative_result.py
@@ -25,7 +25,7 @@ import pretty_midi
 import random
 import torch
 
-def qualitative_result(net, path, rand=True):
+def qualitative_result(net, preds, mean, stdDev, rand=True):
 
     '''
     Plots the ground truth label and network's output of a data sample.
@@ -44,49 +44,60 @@ def qualitative_result(net, path, rand=True):
             prediction (torch.tensor): The output of the net
     '''
 
-    if type(path) != str:
-        raise TypeError('Input "path" must be a string')
-    if os.path.exists(path) == False:
-        raise ValueError('Input "path" must be a valid path')
-    if type(rand) != bool:
-        raise TypeError('Input "random" must be a boolean')
+    # if type(path) != str:
+    #     raise TypeError('Input "path" must be a string')
+    # if os.path.exists(path) == False:
+    #     raise ValueError('Input "path" must be a valid path')
+    # if type(rand) != bool:
+    #     raise TypeError('Input "random" must be a boolean')
 
-    # Load sample s[ectrogram and midi
+    # Load sample spectrogram and midi
     
-    if rand:
+    # if rand:
 
-        data_list = os.listdir(os.path.join(path,'data'))
-        data_file = data_list[random.randint(0,len(data_list)-1)]
-        data_sample = np.load(os.path.join(path,'data',data_file))
+    #     data_list = os.listdir(os.path.join(path,'data'))
+    #     data_file = data_list[random.randint(0,len(data_list)-1)]
+    #     data_sample = np.load(os.path.join(path,'data',data_file))
 
-        midi = pretty_midi.PrettyMIDI(os.path.join(path,'labels',data_file[:-4]+'.mid')) # assume data_file ends with a three letter file type
-        label_sample = midi.get_piano_roll(fs=31)[21:109,:124]
-        label_sample = np.where(label_sample > 0, 1, 0)
+    #     midi = pretty_midi.PrettyMIDI(os.path.join(path,'labels',data_file[:-4]+'.mid')) # assume data_file ends with a three letter file type
+    #     label_sample = midi.get_piano_roll(fs=31)[21:109,:124]
+    #     label_sample = np.where(label_sample > 0, 1, 0)
         
-    else:
+    # else:
 
-        len_of_path = len(path)
-        for i in range(len_of_path):
-            if path[-1] == '/' or path[-1] == '\\':
-                path = path[:-1]
-            else:
-                break
-        for i in range(len(path)-1,-1,-1):
-            if path[i] == '/' or path[i] == '\\':
-                break
+    #     len_of_path = len(path)
+    #     for i in range(len_of_path):
+    #         if path[-1] == '/' or path[-1] == '\\':
+    #             path = path[:-1]
+    #         else:
+    #             break
+    #     for i in range(len(path)-1,-1,-1):
+    #         if path[i] == '/' or path[i] == '\\':
+    #             break
 
-        data_file = path
-        data_sample = np.load(path)
-        midi = pretty_midi.PrettyMIDI(path[:i-5]+'/labels'+path[i:-4]+'.mid') # assume data_file ends with a three letter file type
-        label_sample = midi.get_piano_roll(fs=31)[21:109,:124]
-        label_sample = np.where(label_sample > 0, 1, 0)
+    data_file = path
+    data_sample = np.load(path)
+    midi = pretty_midi.PrettyMIDI(path[:i-5]+'/labels'+path[i:-4]+'.mid') # assume data_file ends with a three letter file type
+    label_sample = midi.get_piano_roll(fs=31)[21:109,:124]
+    label_sample = np.where(label_sample > 0, 1, 0)
+
+    print(label_sample.shape)
+    label_sample = label_sample[None, :, :]
+    print(label_sample.shape) 
         
     # Get net prediction
 
     input_tensor = (torch.from_numpy(data_sample)).unsqueeze(0).unsqueeze(0)
+    print(input_tensor.shape)
+    #index = torch.tensor([0, 126])
+    #input_tensor = torch.index_select(input_tensor, , index)
+    input_tensor = input_tensor[:, :, :, 0:126]
+    input_tensor = (input_tensor - mean)/stdDev
+    print(input_tensor.shape) 
     device = next(net.parameters()).device
-    prediction = net(input_tensor.to(device))
-    prediction = torch.where(prediction > 0, 1, 0)
+    prediction, _ = net(input_tensor.to(device))
+    print(prediction)
+    prediction = torch.where(prediction > 0.5, 1, 0)
 
     # Visualize ground truth label and model prediction
     
@@ -95,14 +106,16 @@ def qualitative_result(net, path, rand=True):
     plt.subplot(211).set_title('Ground Truth MIDI')
     plt.subplot(211).set_xlabel('Frame')
     plt.subplot(211).set_ylabel('MIDI Note')
-    plt.imshow(label_sample, cmap="Blues", aspect="auto", origin='lower') # auto makes each box rectangular
+    plt.imshow(label_sample[0], cmap="Blues", aspect="auto", origin='lower') # auto makes each box rectangular
     plt.colorbar()
     plt.subplot(212)
     plt.subplot(212).set_title('Network Predictions')
     plt.subplot(212).set_xlabel('Frame')
     plt.subplot(212).set_ylabel('MIDI Note')
-    plt.imshow(prediction.squeeze(0).detach().numpy(), cmap="Blues", aspect="auto", origin='lower') # auto makes each box rectangular
+    plt.imshow(prediction.squeeze(0).cpu().detach().numpy(), cmap="Blues", aspect="auto", origin='lower') # auto makes each box rectangular
     plt.colorbar()
     plt.show()
+
+    label_sample = label_sample[None, :, :]
 
     return data_file, label_sample, prediction
